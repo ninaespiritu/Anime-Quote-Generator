1c3a23f8818bedb6be2fcd0d5f326d488ee7f769
Nina Espiritu <ninaangela.espiritu@gmail.com>
2022-01-25 16:04:28 +0000

    Add request to Animechan API

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css';
+import { useState } from 'react'
 
 const App = () => {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+
+    const [joke, setJoke] = useState({})
+
+    const handleFetch = async () => {
+        const response = await fetch("https://animechan.vercel.app/api/random")
+        const data = await response.json()
+        console.log(data)
+        setJoke(data)
+    }
+
+    return (
+        <div>
+            <h1>Anime Quotes</h1>
+            <button onClick={handleFetch}>Get a Random Quote</button>
+            <div>{joke.quote}</div>
+            <div>{joke.character}</div>
+            <div>{joke.anime}</div>
+        </div>
+    );
 }
 
 export default App;
